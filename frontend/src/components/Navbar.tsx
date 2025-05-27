f448c973d73ec67f55ec2d5ab60b8f2565899430
Yominecraft3 <yominecraft3@gmail.com>
2025-05-27 19:44:57 +1200

    removed all styles from nav

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav style={styles.nav}>
-      <ul style={styles.ul}>
+    <nav>
+      <ul>
         <li><Link to="/" >Home</Link></li>
         <li><Link to="/list" >List</Link></li>
         <li><Link to="/search" >Search</Link></li>
