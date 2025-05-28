2810e179838efe4a750131ca31970d2ab5385c25
Yomi8 <19049@jhc.school.nz>
2025-05-29 09:08:43 +1200

    Changed Auth0 settings for new domain

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import App from './App.tsx'
+import App from './App.tsx';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = 'dev-id5fm2bqd16i1nrv.au.auth0.com';
 const clientId = '6K1X0Vpq494HT8sX63LtewqCCEYL5kFM';
-const serverIp = '109.123.227.242';
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -15,10 +13,10 @@ createRoot(document.getElementById('root')!).render(
       domain={domain}
       clientId={clientId}
       authorizationParams={{
-        redirect_uri: `http://${serverIp}:3000/callback`,
+        redirect_uri: 'https://yomi16.nz',
       }}
     >
       <App />
     </Auth0Provider>
   </StrictMode>
-)
+);
