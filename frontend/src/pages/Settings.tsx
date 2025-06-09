f87c5c64227dab0b8b9e989fbfe61b17b57f51c6
Yomi8 <19049@jhc.school.nz>
2025-06-09 14:28:48 +1200

    fixed settings page formatting

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -3,9 +3,11 @@ import SpotifyUpload from "../components/JSONUpload";
 
 export default function Settings() {
   return (
-  <h1>Settings Page</h1>
-  <div className="mt-4">
-    <SpotifyUpload />
-  </div>
+    <>
+      <h1>Settings Page</h1>
+      <div className="mt-4">
+        <SpotifyUpload />
+      </div>
+    </>
   );
 }
