d001d273a63e779d9b09ee5f5825cdae7d1acf5d
Yomi8 <19049@jhc.school.nz>
2025-06-16 14:36:19 +1200

    added needed parts of JWT back

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_cors import CORS
 import mysql.connector
 import json
