a89144720f77be946c9664a4b066478887153f8c
Yomi8 <19049@jhc.school.nz>
2025-06-27 10:25:03 +1200

    use dotenv for storing enviroment variables

--- backend/tasks.py
+++ backend/tasks.py
@@ -5,6 +5,9 @@ from spotify_client import sp
 import mysql.connector.pooling
 import requests
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
