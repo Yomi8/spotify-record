62d6de5c7c4c85ccb353172ff81839d2b5789a27
Yominecraft3 <yominecraft3@gmail.com>
2025-08-04 16:57:25 +1200

    file format

--- frontend/src/pages/Lists.tsx
+++ frontend/src/pages/Lists.tsx
@@ -3,7 +3,7 @@ import backgroundImg from '../assets/images/background.jpg';
 
 
 import buttonIMG1 from '../assets/images/buttons/buttonIMG1.jpg';
-import buttonIMG2 from '../assets/images/buttons/buttonIMG2.jpg';
+import buttonIMG2 from '../assets/images/buttons/buttonIMG2.png';
 import buttonIMG3 from '../assets/images/buttons/buttonIMG3.jpg';
 import buttonIMG4 from '../assets/images/buttons/buttonIMG4.jpg';
 import buttonIMG5 from '../assets/images/buttons/buttonIMG5.jpg';
